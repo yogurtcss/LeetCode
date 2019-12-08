818814551bce6899b4c4bd083db9d970f1ddb396
yogurtcss <1806631955@qq.com>
2019-12-08 20:13:24 +0800

    upload

---
+++ Easy/src/pid9/灵魂画师牧码/Solution.java
@@ -0,0 +1,15 @@
+package pid9.灵魂画师牧码;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        int cur = 0;
+        int num = x;
+        while(num != 0) {
+            cur = cur * 10 + num % 10;
+            num /= 10;
+        }
+        return cur == x;
+    }
+}
