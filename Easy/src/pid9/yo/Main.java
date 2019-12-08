cd1d1f3e326a1cb971a41d1c2e511e958b7495e4
yogurtcss <1806631955@qq.com>
2019-12-08 20:23:56 +0800

    upload

--- Easy/src/pid9/yo/Main.java
+++ Easy/src/pid9/yo/Main.java
@@ -1,13 +1,15 @@
 package pid9.yo;
 
 import pid9.yo.Solution;
+import pid9.yo.Solution2;
 
 public class Main {
     public static void test(){
-        int x = 0;
-        Solution solu = new Solution();
+        int x = -111;
+        //Solution solu = new Solution();
+        Solution solu2 = new Solution();
         long begin = System.currentTimeMillis();
-        boolean b = solu.isPalindrome(x);
+        boolean b = solu2.isPalindrome(x);
         long end = System.currentTimeMillis();
 
         System.out.println( b );
