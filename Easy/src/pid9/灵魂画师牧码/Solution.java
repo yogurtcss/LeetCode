86b2c5f4552c945a680c6ccf78133aafc2e1eaeb
yogurtcss <1806631955@qq.com>
2019-12-08 20:14:05 +0800

    upload

--- Easy/src/pid9/灵魂画师牧码/Solution.java
+++ Easy/src/pid9/灵魂画师牧码/Solution.java
@@ -1,5 +1,14 @@
 package pid9.灵魂画师牧码;
 
+/*
+如果是负数则一定不是回文数，直接返回 false
+如果是正数，则将其倒序数值计算出来，然后比较和原数值是否相等
+如果是回文数则相等返回 true，如果不是则不相等 false
+比如 123 的倒序 321，不相等；121 的倒序 121，相等
+
+*  */
+
+
 class Solution {
     public boolean isPalindrome(int x) {
         if(x < 0)
