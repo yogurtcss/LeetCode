60e0d046de50cc7eca781a64cfca8cd9943cc7e7
yogurtcss <1806631955@qq.com>
2019-12-14 10:58:52 +0800

    upload

--- Easy/src/pid20/yo/Main.java
+++ Easy/src/pid20/yo/Main.java
@@ -1,12 +1,18 @@
 package pid20.yo;
 
+import pid20.王尼玛.Solution;
+import pid20.yo.Solution3;
+
 public class Main {
     public static void test(){
-        String s = "([)";
-        //Solution solu = new Solution();
-        Solution2_notFinished solu2 = new Solution2_notFinished();
+        String s = "()";
+
+        Solution solu = new Solution();
+        //Solution2_notFinished solu2 = new Solution2_notFinished();
+        Solution3 solu3 = new Solution3();
+
         long begin = System.currentTimeMillis();
-        boolean rst = solu2.isValid(s);
+        boolean rst = solu3.isValid(s);
         long end = System.currentTimeMillis();
         System.out.println( rst );
         System.out.println( "------------" ); //换行
