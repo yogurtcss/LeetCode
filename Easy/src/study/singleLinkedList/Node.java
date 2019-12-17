25cefcef3323317a78a543856bef75efade61561
yogurtcss <1806631955@qq.com>
2019-12-17 22:53:23 +0800

    upload

--- Easy/src/study/singleLinkedList/Node.java
+++ Easy/src/study/singleLinkedList/Node.java
@@ -3,35 +3,22 @@ package study.singleLinkedList;
 //单链表中，【某个结点】的类
 
 public class Node {
+    /* 为了方便，这两个变量都使用public 而不用private，
+    * 这样就不用写set、get方法了
+    * */
+
     //某个结点的数据值( Object类型，或基本数据类型 int等 )
-    Object data;
+    public int val;
     //此结点的 “下一个结点”的实例对象 的引用
-    Node next;
+    public Node next;
 
-    //一般结点的构造方法
-    public Node(Object data, Node next ){
-        this.data = data;
-        this.next = next;
+    //构造方法，在构造时就能给成员变量 val赋值
+    public Node( int val ){
+        this.val = val;
     }
 
-    //构造方法：在构造时就能给数据域data赋值
-    public Node(Object data) {
-        this.data = data;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
+    public Node( int val, Node next ){
+        this.val = val;
         this.next = next;
     }
 
