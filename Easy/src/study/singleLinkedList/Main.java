859e157bd660ff75d1ce7e23a115c56a06643737
yogurtcss <1806631955@qq.com>
2019-12-18 22:00:07 +0800

    upload

--- Easy/src/study/singleLinkedList/Main.java
+++ Easy/src/study/singleLinkedList/Main.java
@@ -7,6 +7,12 @@ public class Main {
     public static void test(){
         SingleLinkedList list = new SingleLinkedList();
         list.addAtHead( 233 );
+        //list.addAtTail( 2 );
+
+        //list.clear();
+
+        //list.addAtIndex( 1, 456 );
+        list.deleteAtIndex( 0 );
         System.out.println( list.get(0) );
     }
 
